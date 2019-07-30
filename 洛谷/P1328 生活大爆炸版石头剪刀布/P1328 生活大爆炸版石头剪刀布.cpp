ea8ed2024298f719f5812e6621080ba7cab8abc5
邢家朋 <47733616+xingjiapeng@users.noreply.github.com>
2019-07-30 12:21:11 +0800

    Update P1328 生活大爆炸版石头剪刀布.cpp

--- 洛谷/P1328 生活大爆炸版石头剪刀布/P1328 生活大爆炸版石头剪刀布.cpp
+++ 洛谷/P1328 生活大爆炸版石头剪刀布/P1328 生活大爆炸版石头剪刀布.cpp
@@ -4,52 +4,37 @@ int a[2001],b[2001];
 
 int main(){
 	int n,as,bs;
-
 	cin>>n>>as>>bs;
-
 	int ac=0,bc=0;
-
 	int az=0,bz=0;
-
 	for(int i=1;i<=as;i++) cin>>a[i];
 	for(int i=1;i<=bs;i++) cin>>b[i];
 	for(int i=1;i<=n;i++){
 		if(az==as) az=1;
 		else az++;
 		if(bz==bs) bz=1;
-
 		else bz++;
-
 		if(a[az]==0&&b[bz]==1){
-
 			bc++;
-
 			continue;
 		}
 		if(a[az]==1&&b[bz]==0){
-
 			ac++;
 			continue;
 		}
 		if(a[az]==1&&b[bz]==2){
-
 			bc++;
-
 			continue;
 		}
 		if(a[az]==2&&b[bz]==1){
-
 			ac++;
 			continue;
 		}
 		if(a[az]==2&&b[bz]==0){
-
 			bc++;
-
 			continue;
 		}
 		if(a[az]==0&&b[bz]==2){
-
 			ac++;
 			continue;
 		}
